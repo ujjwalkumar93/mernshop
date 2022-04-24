3f48ef71b20810cb457d53e800ec03cce32dcf3c
ujjwalkumar93 <pathakujjwal93@gmail.com>
2022-04-25 01:22:19 +0530

    fixed user.js file

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const crypto = require("crypto");
-const uuidv1 = require("uuid/v1")
+const uuidv1 = require("uuid/v1");
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -29,8 +29,7 @@ const userSchema = new mongoose.Schema({
     encry_password: {
         type: String,
         required: true,
-        trim: true,
-        required: true
+        trim: true
     },
     salt: String,
     role: {
