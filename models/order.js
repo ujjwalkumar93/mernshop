38d31059efa22df4d57d5870dc5a4611cd5c2ce7
ujjwalkumar93 <pathakujjwal93@gmail.com>
2022-04-25 01:00:00 +0530

    all schema created

---
+++ models/order.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const {ObjectId} = mongoose.Schema;
+
+const ProductCartSchema = new mongoose.Schema({
+    product: {
+        type: ObjectId,
+        ref: "Product"
+    },
+    name: String,
+    count: Number,
+    price: Number
+});
+
+const orderSchema = mongoose.Schema({
+    products: [ProductCartSchema],
+    transaction_id: {},
+    amount: {type: Number},
+    address: String,
+    updated: Date,
+    user: {
+        type: ObjectId,
+        ref: "User"
+    }
+}, {timestamps: true});
+
+const ProductCart = mongoose.model("ProductCart", ProductCartSchema);
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = {Order, ProductCart };
