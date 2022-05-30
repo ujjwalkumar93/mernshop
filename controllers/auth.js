d2794b403d2601a0dbe319bf8efd200a2365f56a
ujjwalkumar93 <pathakujjwal93@gmail.com>
2022-05-30 10:28:20 +0400

    code added for signin,and signout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,67 @@
+const User = require("../models/user");
+const { check, validationResult } = require('express-validator');
+var expressJwt = require("express-jwt");
+var jwt = require('jsonwebtoken');
+
 exports.signout = (req,res) => {
-    res.json({message: "you are signinup"});
+    res.clearCookie("token")
+    res.json({
+        message : "Use signout sucessfully"
+    })
 }
 
 exports.signup = (req,res) => {
-    console.log("REQ BODY: ", req.body);
-    res.json({
-        message: "User Signup"
-    });
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error : errors.array()[0].message
+        })
+    }
+
+    const user = new User(req.body)
+    user.save((err,user) => {
+        if(err){
+            return err.status(400).json({
+                err: "Not able to save user in DB"
+            })
+        }
+        res.json(user)
+    })
+}
+
+exports.signin = (req,res) => {
+    const errors = validationResult(req)
+    const {email,password} = req.body;
+
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+            error : errors.array()[0].msg
+        })
+    }
+
+    User.findOne({email}, (err,user) => {
+        if(err || !user){
+            return res.status(400).json({
+                error: "User email does not exists"
+            })
+        }
+        if(!user.authenticate(password)){
+            console.log("******************")
+            console.log(user.authenticate(password))
+            return res.status(401).json({
+                error : "Email and passwor do not match"
+            })
+        }
+
+        // create token
+        const token = jwt.sign({_id: user._id},"ujjwal")
+
+        // put token in cookie
+        res.cookie("token",token,{expire: new Date() + 9999});
+
+        // send responce to frontend
+        const {_id,name,email,role} = user;
+        return res.json({token,user : {_id,name,email,role}})
+    })
+
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const {signout, signup } = require("../controllers/auth")
+var express = require('express');
+var router = express.Router();
+const {signout, signup, signin } = require("../controllers/auth")
+const { check, validationResult } = require('express-validator');
 
+router.post("/signup",[
+    check("name","name should be atleast 3 char").isLength({min: 3}),
+    check("email","email is required").isEmail(),
+    check("password","password should be atleast 6 char").isLength({min : 6}),
+], signup)
 
-router.post("/signup", signup )
+router.get("/signin",[
+    check("email","email is required").isEmail(),
+    check("password","password should be atleast 6 char").isLength({min : 6}),
+], signin)
 router.get("/signout", signout);
 
 module.exports = router;
